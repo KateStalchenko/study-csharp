b8b5133e9db0c62b9440b7a56b7d8c25510416b2
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2018-08-04 21:33:23 +0300

    Add L-13 FindTheMinimum

---
+++ ULearn/C# Basics/L13-FindTheMinimum/L13-FindTheMinimum/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите метод Min, который бы вычислял минимум из элементов массива. */
+
+using System;
+
+namespace L13_FindTheMinimum
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Min(new[] { 3, 6, 2, 4 }));
+            Console.WriteLine(Min(new[] { "B", "A", "C", "D" }));
+            Console.WriteLine(Min(new[] { '4', '2', '7' }));
+        }
+
+        public static object Min(Array arr)
+        {
+            Array.Sort(arr);
+            return arr.GetValue(0);
+        }
+    }
+}
