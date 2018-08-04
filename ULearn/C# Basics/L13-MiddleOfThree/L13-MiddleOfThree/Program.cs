401f22508b89e39d4ecaaf686c1925de5d3d2976
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2018-08-04 21:32:20 +0300

    Add L-13 MiddleOfThree

---
+++ ULearn/C# Basics/L13-MiddleOfThree/L13-MiddleOfThree/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Помните задачу "Среднее трех"? Пришло время повторить ее для общего случая! */
+
+using System;
+
+namespace L13_MiddleOfThree
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(MiddleOfThree(2, 5, 4));
+            Console.WriteLine(MiddleOfThree(3, 1, 2));
+            Console.WriteLine(MiddleOfThree(3, 5, 9));
+            Console.WriteLine(MiddleOfThree("B", "Z", "A"));
+            Console.WriteLine(MiddleOfThree(3.45, 2.67, 3.12));
+        }
+
+        static IComparable MiddleOfThree(IComparable a, IComparable b, IComparable c)
+        {
+            if ((b.CompareTo(a) <= 0 && a.CompareTo(c) <= 0) || (c.CompareTo(a) <= 0 && a.CompareTo(b) <= 0))
+                return a;
+            else if ((a.CompareTo(b) <= 0 && b.CompareTo(c) <= 0) || (c.CompareTo(b) <= 0 && b.CompareTo(a) <= 0))
+                return b;
+            else
+                return c;
+        }
+
+
+        //static IComparable MiddleOfThree(IComparable a, IComparable b, IComparable c)
+        //{
+        //    IComparable[] arr = { a, b, c };
+        //    Array.Sort(arr);
+        //    return arr[1];
+        //}
+    }
+}
