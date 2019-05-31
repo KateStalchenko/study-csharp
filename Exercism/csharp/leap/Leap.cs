73a0d3fc6c85edd93b3184f62109766b2fb4e01b
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2019-05-31 21:22:03 +0300

    Add exercism Tasks

--- Exercism/csharp/leap/Leap.cs
+++ Exercism/csharp/leap/Leap.cs
@@ -1,9 +1,13 @@
-using System;
-
 public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+
+        if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
