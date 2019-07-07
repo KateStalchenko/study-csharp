b5b51979d7491e1bd951c45b42243f5850d198bb
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2019-07-07 18:05:50 +0300

    Update

--- Exercism/csharp/leap/Leap.cs
+++ Exercism/csharp/leap/Leap.cs
@@ -2,12 +2,6 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-
-        if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0)
-        {
-            return true;
-        }
-
-        return false;
+        return year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
     }
 }
