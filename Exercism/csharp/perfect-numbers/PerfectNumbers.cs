d1b2c9f192e0b899c122ab10bc8dad3d8e924063
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2019-07-07 18:08:06 +0300

    Add Perfect Numbers Task

---
+++ Exercism/csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var factors = new List<int>();
+        for (var i = 1; i < int.MaxValue; i++)
+        {
+            if (i >= number)
+            {
+                break;
+            }
+
+            if (number % i == 0)
+            {
+                factors.Add(i);
+            }
+        }
+
+        var sum = factors.Sum();
+        return sum == number ? Classification.Perfect : sum > number ? Classification.Abundant : Classification.Deficient;
+    }
+}
