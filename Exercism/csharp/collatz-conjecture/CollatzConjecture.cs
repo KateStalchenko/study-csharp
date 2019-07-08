74bf665967d3f02a1689ea7dc283f1e129e386f1
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2019-07-08 12:27:03 +0300

    Add collatz-conjecture Task

---
+++ Exercism/csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentException();
+        }
+
+        var stepsNumber = 0;
+
+        while (number != 1)
+        {
+            if (number == 1)
+            {
+                break;
+            }
+
+            if (number % 2 == 0)
+            {
+                number /= 2;
+                stepsNumber++;
+            }
+
+            else
+            {
+                number = number * 3 + 1;
+                stepsNumber++;
+            }
+        }
+
+        return stepsNumber;
+    }
+}
