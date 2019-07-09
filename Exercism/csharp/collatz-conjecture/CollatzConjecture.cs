28dcbd6392c6b5b8eb7bca10d88497357e11dcb8
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2019-07-09 20:46:37 +0300

    Update Collatz-Conjecture Task

--- Exercism/csharp/collatz-conjecture/CollatzConjecture.cs
+++ Exercism/csharp/collatz-conjecture/CollatzConjecture.cs
@@ -6,29 +6,15 @@ public static class CollatzConjecture
     {
         if (number <= 0)
         {
-            throw new ArgumentException();
+            throw new ArgumentException("Number is less or equal to zero.");
         }
 
         var stepsNumber = 0;
 
         while (number != 1)
         {
-            if (number == 1)
-            {
-                break;
-            }
-
-            if (number % 2 == 0)
-            {
-                number /= 2;
-                stepsNumber++;
-            }
-
-            else
-            {
-                number = number * 3 + 1;
-                stepsNumber++;
-            }
+            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
+            stepsNumber++;
         }
 
         return stepsNumber;
