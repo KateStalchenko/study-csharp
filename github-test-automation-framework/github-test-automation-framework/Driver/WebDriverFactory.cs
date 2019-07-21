c2f883b8437c16c790ceb408f64eb66d107e99c1
Katsiaryna Stalchanka <e.stalchanka@gmail.com>
2019-07-21 22:24:59 +0300

    Init TA Framework

---
+++ github-test-automation-framework/github-test-automation-framework/Driver/WebDriverFactory.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace github_test_automation_framework.Driver
+{
+    class WebDriverFactory
+    {
+        private static IWebDriver driver;
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (driver == null)
+                {
+                    throw new NullReferenceException("You need to initialize driver");
+                }
+                return driver;
+            }
+
+            private set
+            {
+                driver = value;
+            }
+        }
+    }
+}
